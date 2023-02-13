aa123321dc102413287260850e5462cd3114e3f8
Volodymyr Nazaryshyn <volodimirnazarisin@gmail.com>
2023-02-13 16:07:44 +0200

    project start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+    return (
+        <>
+            <header>
+                <a href="/">Home</a>
+                <a href="/posts">Posts</a>
+                <a href="/about">About</a>
+            </header>
+            <div>
+                <h1>Get started with React-Router 6</h1>
+            </div>
+        </>
+    )
+}
