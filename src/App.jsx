14ca28749c212c9e8738b72f3e7b3a89bc8b7a6e
Volodymyr Nazaryshyn <volodimirnazarisin@gmail.com>
2023-02-13 17:18:11 +0200

    Params and useParams

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Blog } from './pages/Blog';
+import { CreatePost } from './pages/CreatePost';
+import { EditPost } from './pages/EditPost';
+import { SinglePage } from './pages/SinglePage';
 import { NotFound } from './pages/NotFound';
 
 import { Layout } from './components/Layout';
@@ -15,6 +18,9 @@ export default function App() {
                     <Route index element={<Home />} />
                     <Route path="about" element={<About />} />
                     <Route path="posts" element={<Blog />} />
+                    <Route path="posts/:id" element={<SinglePage />} />
+                    <Route path="posts/:id/edit" element={<EditPost />} />
+                    <Route path="posts/new" element={<CreatePost />} />
                     <Route path="*" element={<NotFound />} />
                 </Route>
             </Routes>
---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,7 @@
+export const CreatePost = () => {
+    return (
+        <div>
+            <h1>Create a post</h1>
+        </div>
+    )
+}
