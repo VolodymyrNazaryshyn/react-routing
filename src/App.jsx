319bfa2da6e86b1222e7771fc9833a98c8441882
Volodymyr Nazaryshyn <volodimirnazarisin@gmail.com>
2023-02-13 22:19:33 +0200

    Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { About } from './pages/About';
@@ -7,23 +7,35 @@ import { CreatePost } from './pages/CreatePost';
 import { EditPost } from './pages/EditPost';
 import { SinglePage } from './pages/SinglePage';
 import { NotFound } from './pages/NotFound';
+import { Login } from './pages/Login';
 
 import { Layout } from './components/Layout';
+
+import { RequireAuth } from './hoc/RequireAuth';
+import { AuthProvider } from './hoc/AuthProvider';
 
 export default function App() {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="about" element={<About />} />
-                    <Route path="posts" element={<Blog />} />
-                    <Route path="posts/:id" element={<SinglePage />} />
-                    <Route path="posts/:id/edit" element={<EditPost />} />
-                    <Route path="posts/new" element={<CreatePost />} />
-                    <Route path="*" element={<NotFound />} />
-                </Route>
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="about" element={<About />} />
+                        <Route path="about-us" element={<Navigate to="/about" replace />} />
+                        <Route path="posts" element={<Blog />} />
+                        <Route path="posts/:id" element={<SinglePage />} />
+                        <Route path="posts/:id/edit" element={<EditPost />} />
+                        <Route path="posts/new" element={
+                            <RequireAuth>
+                                <CreatePost />
+                            </RequireAuth>
+                        } />
+                        <Route path="login" element={<Login />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
+                </Routes>
+            </AuthProvider>
         </>
     )
 }
--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,9 +1,13 @@
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 export const SinglePage = () => {
-    const {id} = useParams();
+    const { id } = useParams();
+    const navigate = useNavigate();
     const [post, setPost] = useState(null);
+
+    const goBack = () => navigate(-1);
+    const goHome = () => navigate('/', { replace: true });
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -15,6 +19,9 @@ export const SinglePage = () => {
         <div>
             {post && (
                 <>
+                    <button onClick={goBack}>Go back</button>
+                    {/* Bad approach */}
+                    <button onClick={goHome}>Go home</button>
                     <h1>{post.title}</h1>
                     <p>{post.body}</p>
                     <Link to={`/posts/${id}/edit`}>Edit this post</Link>
